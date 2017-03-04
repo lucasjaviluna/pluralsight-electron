3bf50dcc1c479889f17f9b38af20817c7c4fb986
lucasjaviluna <lucasjaviluna@gmail.com>
2017-03-04 20:58:55 -0300

    Tray

--- src/main.js
+++ src/main.js
@@ -3,11 +3,35 @@ const app = electron.app;
 const ipc = electron.ipcMain;
 const BrowserWindow = electron.BrowserWindow;
 const Menu = electron.Menu;
+const Tray = electron.Tray;
 
+const path = require('path');
 const countdown = require('./countdown');
 const windows = [];
 
 app.on('ready', () => {
+  //Tray
+  const tray = new Tray(path.join('src', 'tray.png'));
+
+  //contectual menu
+  const contextMenu = Menu.buildFromTemplate([
+      {
+        label: 'Wow',
+        click: _ => console.log('Wow clicked!')
+      },
+      {
+        label: 'Awesome',
+        click: _ => console.log('Awesome clicked!')
+      },
+      {
+        label: 'Exit',
+        click: _ => app.quit()
+      }
+  ]);
+
+  tray.setContextMenu(contextMenu);
+  tray.setToolTip('Electron Rules!');
+
   //Menu
   const name = electron.app.getName();
   const template = [
@@ -36,8 +60,8 @@ app.on('ready', () => {
   ];
   const menu = Menu.buildFromTemplate(template);
   Menu.setApplicationMenu(menu);
-
-  [1, 2, 3].forEach(_ => {
+  //[1, 2, 3]
+  [1].forEach(_ => {
     let win = new BrowserWindow({
       height: 600,
       width: 650
